e4100c190e24d6d742e46ccc6b241acc2895ad7b
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-10-20 17:02:04 +0100

    Feat: adding components - creating LandingPage

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+const Logo = () => {
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image src="/logo.svg" alt="Logo" height={40} width={40} />
+      <p className={cn("font-semibold", font.className)}>Jotion</p>
+    </div>
+  );
+};
+
+export default Logo;
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <main>Jotion main page</main>
-  )
-}
