c3d3cbf480c88f8e2cfa0690ca186ce4cd6fae92
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-10-23 13:05:08 +0100

    Feat: added SideBar animations and usabilitty

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,7 @@
 import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
+import Navigation from "./_components/navigation";
 
 const MainPageLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -21,6 +22,7 @@ const MainPageLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
       <main className="flex-1 h-full overflow-y-auto">{children}
       </main>
     </div>
