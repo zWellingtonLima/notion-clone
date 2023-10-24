9fc8d23a529951a9415eabc83e3a988f4856f099
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-10-25 00:12:48 +0100

    Feat: added UserItem component instead string "Action items"

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,6 +6,7 @@ import { useMediaQuery } from "usehooks-ts";
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
+import { UserItem } from "./user-item";
 
 const Navigation = () => {
   const pathname = usePathname();
@@ -112,7 +113,7 @@ const Navigation = () => {
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Action items</p>
+            <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
@@ -126,7 +127,7 @@ const Navigation = () => {
       <div
         ref={navbarRef}
         className={cn(
-          "absolute top-0 z-50 left-60 w-[calc(100%-240px)]",
+          "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "left-0 w-full"
         )}
