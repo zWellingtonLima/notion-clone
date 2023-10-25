89a8ae5fcb9d097d3c915e898704baea7bf4d35e
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-10-25 23:36:26 +0100

    Feat: added sonner package and {}

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
 
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
