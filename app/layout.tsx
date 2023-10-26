eb53fa5f0aa0c724127836c39d70275f31cbeb13
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-10-26 21:13:35 +0100

    Feat: added schema at convex folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="jotion-theme"
           >
+            <Toaster position="bottom-center"/>
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
