a87d773940c50baa0682ead21330bd05eb587591
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-10-30 13:08:56 +0000

    Fix: fixed router.push('documents') to router.push('/documents')

--- app/(main)/_components/document-list.tsx
+++ app/(main)/_components/document-list.tsx
@@ -37,7 +37,7 @@ export const DocumentList = ({
   });
 
   const onRedirect = (documentId: string) => {
-    router.push(`documents/${documentId}`);
+    router.push(`/documents/${documentId}`);
   };
 
   if (documents === undefined) {
