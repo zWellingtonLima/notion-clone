9fded522ea404b6730d5849e5c1d414a921c0849
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-11-01 13:48:45 +0000

    Feat: addding toolbar component to main page

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,39 @@
-const DocumentIdPage = () => {
-  return (
-    <div>DocumentId</div>
-  )
+"use client";
+
+import { useQuery } from "convex/react";
+
+import { Id } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+
+import Toolbar from "@/components/toolbar";
+
+interface DocumentIdProps {
+  params: {
+    documentId: Id<"documents">;
+  };
 }
 
-export default DocumentIdPage
+const DocumentIdPage = ({ params }: DocumentIdProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  if (document === null) {
+    return <div>Not found</div>
+  }
+
+  return (
+    <div className="pb-40">
+        <div className='h-[35vh]'/>
+        <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
+          <Toolbar initialData={document} />
+        </div>
+      </div>
+  );
+};
+
+export default DocumentIdPage;
---
+++ components/toolbar.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { useState, ElementRef, useRef } from "react";
+import { ImageIcon, Smile, X } from "lucide-react";
+import { useMutation } from "convex/react";
+
+import { Doc } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+
+import { Button } from "./ui/button";
+import { IconPicker } from "./icon-picker";
+
+interface ToolbarProps {
+  initialData: Doc<"documents">;
+  preview?: boolean;
+}
+
+const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+  const inputRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(initialData.title);
+
+  const update = useMutation(api.documents.update);
+
+  const enableInput = () => {
+    if (preview) return;
+
+    setIsEditing(true);
+    setTimeout(() => {
+      setValue(initialData.title);
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const disableInput = () => setIsEditing(false);
+
+  const onInput = (value: string) => {
+    setValue(value);
+    update({
+      id: initialData._id,
+    })
+  }
+
+  return (
+    <div className="pl-[54px] group relative">
+      {!!initialData.icon && !preview && (
+        <div className="flex items-center gap-x-2 group/icon pt-6">
+          <IconPicker onChange={() => {}}>
+            <p className="text-6xl hover:opacity-75 transition">
+              {initialData.icon}
+            </p>
+          </IconPicker>
+          <Button
+            onClick={() => {}}
+            className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
+            variant="outline"
+            size="icon"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </div>
+      )}
+      {!!initialData.icon && preview && (
+        <p className="text-6xl pt-6">{initialData.icon}</p>
+      )}
+      <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
+        {!initialData.icon && !preview && (
+          <IconPicker asChild onChange={() => {}}>
+            <Button
+              className="text-muted-foreground text-xs"
+              variant="outline"
+              size="sm"
+            >
+              <Smile className="w-4 h-4 mr-2" />
+              Add Icon
+            </Button>
+          </IconPicker>
+        )}
+        {!initialData.converImage && !preview && (
+          <Button
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Add cover
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Toolbar;
